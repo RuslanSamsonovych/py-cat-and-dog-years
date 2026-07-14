051f515ee38c632e17ba765aeaa105f9842cb75f
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2026-07-14 20:54:43 +0300

    implement get_human_age func and tests

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
-    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+    cat_to_human = convert_to_human(cat_age, 15, 9, 4)
+    dog_to_human = convert_to_human(dog_age, 15, 9, 5)
+    return [cat_to_human, dog_to_human]
+
+
+def convert_to_human(
+        animal_age: int, first_year: int, second_year: int, each_year: int
+) -> int:
+    if not isinstance(animal_age, int):
         raise TypeError("Age should be integer")
-
-    age_dict = {"cat_age": cat_age, "dog_age": dog_age}
-    human_age = []
-    for key, value in age_dict.items():
-        if value < 0:
-            raise ValueError("Age should be 0+")
-        if value <= 15:
-            human_age.append(value // 15)
-        elif 16 <= value <= 24:
-            human_age.append(1 + (value - 15) // 9)
-        else:
-            if key == "cat_age":
-                human_age.append(2 + (value - 24) // 4)
-            if key == "dog_age":
-                human_age.append(2 + (value - 24) // 5)
-
-    return human_age
+    if animal_age < 0:
+        raise ValueError("Age should be 0+")
+    if animal_age < first_year:
+        return 0
+    if animal_age < first_year + second_year:
+        return 1
+    return 2 + (animal_age - first_year - second_year) // each_year
